9a936e8f6557d18322cf26669ba3282403f58bc5
ivangrinko-dev <grinko.work.1982@mail.ru>
2023-05-03 22:22:15 +0300

    problem solving

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,59 +1,57 @@
 const fs = require(`fs`);
 
 const path = `./storage/storage.json`;
+class User {
+  path = `./storage/storage.json`
+  getAllUsers() {
+    const data = JSON.parse(fs.readFileSync(this.path));
+    return data;
+  }
 
-function getAllUsers() {
-  const data = JSON.parse(fs.readFileSync(path));
-  return data;
-}
+  getUserById(id) {
+    const data = JSON.parse(fs.readFileSync(this.path));
+    const filtered = data.filter((elem) => elem.id == id);
+    return filtered;
+  }
 
-function getUserById(id) {
-  const data = JSON.parse(fs.readFileSync(path));
-  const filtered = data.filter((elem) => elem.id == id);
-  return filtered;
-}
-
-function createUser(name, surname, email, pwd) {
-  const data = JSON.parse(fs.readFileSync(path));
-  data.push({
-    id: data.length + 1,
-    name,
-    surname,
-    email,
-    pwd,
-  });
-  fs.writeFileSync(path, JSON.stringify(data));
-  return data;
-}
-
-function upUserById(id, name, surname, email, pwd) {
-  const data = JSON.parse(fs.readFileSync(path));
-  const filtered = data.filter((elem) => elem.id != id);
-  if (filtered.length != data.length) {
-    filtered.push({
-      id,
+  createUser(name, surname, email, pwd) {
+    const data = JSON.parse(fs.readFileSync(this.path));
+    data.push({
+      id: data.length + 1,
       name,
       surname,
       email,
       pwd,
     });
-  } else {
-    throw new Error("такого id нет");
+    fs.writeFileSync(path, JSON.stringify(data));
+    return data;
   }
-  fs.writeFileSync(path, JSON.stringify(filtered));
-  return filtered;
-}
 
-function deleteUser(id) {
-  const data = JSON.parse(fs.readFileSync(path));
-  const filtered = data.filter((elem) => elem.id != id);
-  fs.writeFileSync(path, JSON.stringify(filtered));
-  return filtered;
+  upUserById(id, name, surname, email, pwd) {
+    const data = JSON.parse(fs.readFileSync(this.path));
+    const filtered = data.filter((elem) => elem.id != id);
+    if (filtered.length != data.length) {
+      filtered.push({
+        id,
+        name,
+        surname,
+        email,
+        pwd,
+      });
+    } else {
+      throw new Error("такого id нет");
+    }
+    fs.writeFileSync(this.path, JSON.stringify(filtered));
+    return filtered;
+  }
+
+  deleteUser(id) {
+    const data = JSON.parse(fs.readFileSync(this.path));
+    const filtered = data.filter((elem) => elem.id != id);
+    fs.writeFileSync(this.path, JSON.stringify(filtered));
+    return filtered;
+  }
 }
 module.exports = {
-  getAllUsers,
-  getUserById,
-  createUser,
-  upUserById,
-  deleteUser,
+  User,
 };
